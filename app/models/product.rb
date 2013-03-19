63a5d9734b5708ea4adaf12186ca993a460ba7e6
knobli <raffael@santschi.ch>
2013-03-19 15:53:48 +0100

    unit tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
   attr_accessible :available, :description, :image_url, :price, :title
+  validates_presence_of :title, :description, :image_url
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => { :with => %r{\.(gif|jpg|png)$}i, :message => 'must be a URL for GIF, JPG or PNG image.'}
   validate :validate_description_length
   
   #check if the description
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -4,4 +4,9 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "error during validation" do 
+    product = Product.new
+    assert product.invalid?
+  end
 end
