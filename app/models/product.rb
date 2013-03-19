5dcf3de03cd88bd76c304899b46cababfbaf01ea
knobli <raffael@santschi.ch>
2013-03-19 15:44:49 +0100

    add validators

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,20 @@
 class Product < ActiveRecord::Base
   attr_accessible :available, :description, :image_url, :price, :title
+  validate :validate_description_length
+  
+  #check if the description
+  protected
+  def validate_description
+    return unless self.validate_description_length
+    errors.add(:fields,"Description does no match with the criterias")
+  end  
+
+  protected
+  def validate_description_length
+    if ! /[\d\w]{30}/.match(self.description)
+      return false
+    end
+    return true
+  end  
+    
 end
